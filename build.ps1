8cade9b207b4c440f9c1a636649b77528fa262a4
Sergio Santos <santoscamin@gmail.com>
2020-03-17 00:43:00 +0100

    Script for build in PowerShell

---
+++ build.ps1
@@ -0,0 +1,10 @@
+﻿try{
+    pyinstaller --onefile --noconsole -i .\assets\images\icon.ico --add-binary "assets\ui\appUI.ui;." .\app.py
+}catch{
+    try{
+        pip install pyinstaller
+        pyinstaller --onefile --noconsole -i .\assets\images\icon.ico --add-binary "assets\ui\appUI.ui;." .\app.py
+    }catch{
+        Write-Host "ERROR-Cannot build the app, error"
+    }
+}
